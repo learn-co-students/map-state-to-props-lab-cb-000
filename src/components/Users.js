e2c1bf27477567a979b28f05001f6c3eb436df49
derrick b <dbozich1@gmail.com>
2018-11-03 02:55:14 +0000

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Users extends Component {
 
   render() {
+    let users = ''
+    if (this.props.users.length > 0) {
+      users = this.props.users.map(user => {
+        return <li>{user.username}</li>
+      })
+    } else {
+      users = <div></div>
+    }
     return (
       <div>
         <ul>
-          Users!
+          {users}
         </ul>
+        {this.props.users.length}
       </div>
     )
   }
 }
 
 //add mapStateToProps here
+const mapStateToProps = state => {
+  return  state
+}
 
-export default Users
+
+export default connect(mapStateToProps)(Users)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import manageUsers from './reducers/manageUsers'
 
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+const store = createStore(manageUsers, /* preloadedState, */ devToolsEnhancer(
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+));
+
 
 // add imports and code
-
+// const store = createStore(manageUsers)
 
 ReactDOM.render(
   // add imports and code
+  <Provider store={store}>
   <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
